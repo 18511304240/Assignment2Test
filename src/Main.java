3668fe4d42880d05bb10e62925498030b947c458
BoWang <1438135815@qq.com>
2024-05-08 13:23:51 +0800

    test

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("balabala");
-        System.out.println("123");
-        System.out.println("132");
+        System.out.println("Hello");
     }
 }
