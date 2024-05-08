53c51350d4759db1e2a470e34d68acc816c842d1
WAKUWAKU <1939892930@qq.com>
2024-05-08 13:20:43 +0800

    test1

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("balabala");
-        System.out.println("dadadadadad");
+        System.out.println("123");
     }
 }
